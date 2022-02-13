eea073c8bf6fa205ee142a09f80f6959b933d219
Fernando Coutinho <fa2c1@hotmail.com>
2022-02-13 17:27:27 -0300

    Umas das maneiras de se implementar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      
+      status: 1
     }
   }
 
   render(){
     return(
       <div>
-
+        {this.state.status === 1 &&
+          <h1>Bem-vindo ao sistema!</h1>
+        }
+        <div>
+          <h2>Curso ReactJS</h2>
+        </div>
       </div>
     );
   }
